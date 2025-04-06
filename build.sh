198483c265e696f2e61890548a46ac86436abb2c
MorganyJayaprakash <127740096+MorganyJayaprakash@users.noreply.github.com>
2025-04-06 14:05:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -t morgany .
 echo Hyyy
-docker login -u morganyy-p morgany2004
+docker login -u morganyy -p morgany2004
 docker tag morgany morganyy/dev_miniproj
 docker push morganyy/dev_miniproj
 kubectl apply -f  deploy.yaml --validate=false
